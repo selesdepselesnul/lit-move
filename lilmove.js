e00697a494dab6684ef1c2aaac13779ab1af1553
selesdepselesnul <selesdepselesnul@gmail.com>
2017-06-26 10:24:24 +0700

    add test

---
+++ lilmove.js
@@ -0,0 +1,12 @@
+module.exports.listFiles = function() {
+    return [
+      {
+        'items': ['a.txt'],
+        'type': 'txt'
+      },
+      {
+        'items': ['a.png', 'b.png'],
+        'type': 'png'
+      }
+    ];
+  };
--- test/test.js
+++ test/test.js
@@ -1,13 +1,24 @@
 var expect = require('chai').expect;
-describe('Array', function() {
+var lilMove = require('../lilmove');
+var mock = require('mock-fs');
+describe('lilmove', function() {
   before(function() {
     console.log('test');
   });
 
-  describe('#indexOf()', function() {
+  describe('#listFiles()', function() {
     context('when present', function() {
-      it('should return the index where the element first appears in the array', function() {
-        expect(2).to.equal(2);
+      it('should return files element', function() {
+        expect(lilMove.listFiles()).to.deep.equal([
+          {
+            'items': ['a.txt'],
+            'type': 'txt'
+          },
+          {
+            'items': ['a.png', 'b.png'],
+            'type': 'png'
+          }
+        ]);
       });
     });
   });
