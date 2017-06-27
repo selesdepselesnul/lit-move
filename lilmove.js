16521ba92d93d82bf6ac79a88621657ead7053e3
selesdepselesnul <selesdepselesnul@gmail.com>
2017-06-27 09:11:24 +0700

    refactor

--- lilmove.js
+++ lilmove.js
@@ -1,13 +1,9 @@
-const mock = require('mock-fs');
 const fs = require('fs');
-const readChunk = require('read-chunk');
-const fileType = require('file-type');
 const fileExtension = require('file-extension');
 const _ = require('lodash');
 
 module.exports.listFiles = function(dir = '.') {
     const files = fs.readdirSync(dir);
     const filesByExt = _.groupBy(files, fileExtension);
-    console.log(filesByExt);
     return filesByExt;
-  };
+};
