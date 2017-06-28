b5dbc4c7d7480d64002d6e867bb215e9c8bb070a
selesdepselesnul <selesdepselesnul@gmail.com>
2017-06-28 11:33:58 +0700

    add cleanup in test

--- lilmove.js
+++ lilmove.js
@@ -1,9 +1,40 @@
 const fs = require('fs');
 const fileExtension = require('file-extension');
+const mv = require('mv');
 const _ = require('lodash');
 
-module.exports.listFiles = function(dir = '.') {
+
+function listFiles(dir = './') {
     const files = fs.readdirSync(dir);
-    const filesByExt = _.groupBy(files, fileExtension);
+    const filesByExt = _.groupBy(
+                          files,
+                          fileExtension);
     return filesByExt;
+}
+
+module.exports.listFiles = listFiles;
+
+module.exports.move = function(dir = '.', moveDesc) {
+
+  const dirs = listFiles(dir);
+
+  var isError = false;
+  _.forEach(moveDesc, function(value, key) {
+
+    const files = dirs[key];
+
+    _.forEach(files, function(x) {
+
+        mv(dir + x,
+           value + '/' + x,
+           {mkdirp: true},
+           function (err) {
+             if(err)
+              isError = true;
+           });
+    });
+  });
+
+  if(isError) return null;
+  return moveDesc;
 };
--- test/test.js
+++ test/test.js
@@ -1,42 +1,62 @@
-var expect = require('chai').expect;
-var lilMove = require('../lilmove');
-var mock = require('mock-fs');
+const expect = require('chai').expect;
+const lilMove = require('../lilmove');
+const mock = require('mock-fs');
+const mv = require('mv');
+const fs = require('fs');
 
 describe('lilmove', function() {
-
-  before(function() {
-    mock({
-      './a.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
-      './a.txt' : 'fasfasf',
-      './b.png': new Buffer([8, 6, 7, 5, 3, 0, 20])
-    });
-  });
 
   describe('#listFiles()', function() {
     context('when present', function() {
       it('should return files element', function() {
+        mock({
+          './a.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
+          './a.txt' : 'fasfasf',
+          './b.png': new Buffer([8, 6, 7, 5, 3, 0, 20])
+        });
+
         expect(lilMove.listFiles()).to.deep.equal({
           'png': ['a.png', 'b.png'],
           'txt': ['a.txt']
         });
+
+        mock.restore();
       });
     });
   });
 
   describe('#move()', function() {
-    context('when present', function() {
-      it('should return files element', function() {
-        expect(lilMove.listFiles()).to.deep.equal({
-          'png': ['a.png', 'b.png'],
-          'txt': ['a.txt']
-        });
+    context('when success', function() {
+      it('should return move desc', function() {
+
+        const testResourceDir = './test/resource_test/';
+        const moveDesc = {
+            'png' : testResourceDir + 'png',
+            'txt' : testResourceDir + 'txt'
+        };
+
+        expect(lilMove.move(testResourceDir,moveDesc))
+          .to
+          .deep
+          .equal(moveDesc);
       });
     });
   });
 
-
   after(function() {
-    mock.restore();
+    const testResourceDir = './test/resource_test/';
+    mv(testResourceDir + 'png/1.png',
+       testResourceDir + '1.png',
+       function (err) {});
+    mv(testResourceDir + 'png/2.png',
+       testResourceDir + '2.png',
+       function (err) {});
+    mv(testResourceDir + 'txt/1.txt',
+          testResourceDir + '1.txt',
+          function (err) {});
+
+    fs.rmdirSync(testResourceDir + 'png');
+    fs.rmdirSync(testResourceDir + 'txt');
   });
 
 });
